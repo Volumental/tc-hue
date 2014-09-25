57bf752933375a63236950a4259c699d61d8b21b
Samuel Carlsson <samuel.carlsson@volumental.com>
2014-09-25 18:35:19 +0200

    Test now asserts on hue set

--- update_lamp_test.py
+++ update_lamp_test.py
@@ -2,14 +2,10 @@ import update_lamp
 from mock import Mock
 
 class fake_team_city:
-	def __init__(self):
-		self.projects = {}
+	def __init__(self, projects):
+		self.projects = projects
 		self.value = None
-		self.build_type_filter = None
 
-	def add_project(self, id, configs):
-		self.projects[id] = configs
-	
 	def get_from_server(self):
 		return self.value
 
@@ -22,12 +18,19 @@ class fake_team_city:
 		self.value = { u'buildTypes' : { u'buildType': [ { u'id': id } for id in p.keys()] } }
 		return self
 
+	def status(self, success):
+		return 'SUCCESS' if success else 'FAILURE'
+
 	def set_build_type(self, build_type):
-		self.build_type_filter = build_type
+		self.value = {}
+		for id in self.projects:
+			for cfg_id in self.projects[id]:
+				if cfg_id == build_type:
+					s = self.status(self.projects[id][cfg_id]) 
+					self.value =  { u'build': [ { u'status': s } ] }
 		return self
 	
 	def get_all_builds(self):
-		self.value = []
 		return self
 
 	def set_lookup_limit(self, limit):
@@ -41,10 +44,26 @@ def create_bridge_fake(host):
 	bridge.get_light_objects.return_value = [light1]
 	return bridge
 
-def create_team_city_client_fake(config):
-	tc = fake_team_city();
-	tc.add_project('a', { 'config_a': True });
-	return tc
+def assertEqual(expected, actual):
+	assert expected == actual, "Expected %s, but was %s" % (expected, actual)
+
+class fixture:
+	def __init__(self, cfg):
+		self.cfg = cfg
+
+	def test_one_build_failed(self):
+		update_lamp.create_bridge = create_bridge_fake
+		update_lamp.create_team_city_client = lambda config: fake_team_city({ 'a': { 'config_a': False } })
+		update_lamp.update_lamps(self.cfg)
+		
+		assertEqual(63300, light1.hue)	
+
+	def test_no_builds_fail(self):
+		update_lamp.create_bridge = create_bridge_fake
+		update_lamp.create_team_city_client = lambda config: fake_team_city({ 'a': { 'config_a': True } })
+		update_lamp.update_lamps(self.cfg)
+
+		assertEqual(23847, light1.hue)	
 
 def main():
 	cfg = {
@@ -59,11 +78,10 @@ def main():
 			"watch": ["a", "b"]
 		}
 	}
-	update_lamp.create_bridge = create_bridge_fake
-	update_lamp.create_team_city_client = create_team_city_client_fake
-
-	update_lamp.update_lamps(cfg)
-	
+	f = fixture(cfg)
+	f.test_one_build_failed()
+	f.test_no_builds_fail()
 
 if __name__ == "__main__":
     main()
+
