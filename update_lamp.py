b2bb92020c06dc766651881f1a81792288562739
Sumit Tada <sumittada@gmail.com>
2015-04-02 11:31:16 +0200

    Separated updating of build and twitter lamps one shouldn't fail with other, added warning window and sound trigger for catching errors in case of connectivity or api problems

--- update_lamp.py
+++ update_lamp.py
@@ -8,6 +8,9 @@ from tweepy import API
 from datetime import datetime, timedelta
 from email.utils import parsedate_tz
 from time import mktime, sleep, strptime
+from warning_and_alarm import WarningAndAlarm
+import sys
+from urllib2 import URLError
 
 class Color:	
 	def __init__(self, color_string):
@@ -39,6 +42,8 @@ class Color:
 		
 		self.hue = hue * 60.0
 
+alarm = WarningAndAlarm()
+
 def set_color(bridge, color, light_ids=None):
 	lights = bridge.get_light_objects()
 	for light in lights:
@@ -65,66 +70,91 @@ def create_team_city_client(config):
 		tc[u'host'], int(tc[u'port']))
 
 def create_bridge(host):
+	print "Trying hub with address:", host
 	return Bridge(host)
 
+	       
+
+def update_build_lamps(config, bridge):
+			tc = create_team_city_client(config)
+			try:
+				all_projects = tc.get_all_projects().get_from_server()
+			except URLError:
+				alarm.trigger()
+			else:
+				watched = config[u'teamcity'][u'watch'];
+
+				ok_projects = []
+				for p in all_projects[u'project']:
+					id = p[u'id'];
+					try:
+						project = tc.get_project_by_project_id(id).get_from_server()
+					except URLError:
+						alarm.trigger(2)
+					else:			
+						if id in watched:
+							statuses = []	
+							for build_type in project[u'buildTypes'][u'buildType']:
+								b = tc.get_all_builds().set_build_type(build_type[u'id']).set_lookup_limit(2).get_from_server()
+								if u'build' in b:
+									status = b[u'build'][0][u'status']
+									print b[u'build'][0][u'buildTypeId'], status
+									statuses.append(status)
+					
+							ok_projects.append(not 'FAILURE' in statuses)
+
+				on(bridge)
+				if all(ok_projects):
+					set_color(bridge, Color(config[u'colors'][u'success']), config[u'groups'][u'build_lights'][u'ids'])
+				else:
+					set_color(bridge, Color(config[u'colors'][u'fail']), config[u'groups'][u'build_lights'][u'ids'])
+
+def update_twitter_lamps(config, bridge):
+		# Check if have a tweet in the last 5 minutes on twitter. If yes, let the twitter light go bananas.
+		auth = OAuthHandler(config[u'twitter_settings'][u'consumer_key'], config[u'twitter_settings'][u'consumer_secret'])
+		auth.set_access_token(config[u'twitter_settings'][u'access_token'],config[u'twitter_settings'][u'access_token_secret'])
+		api = API(auth)
+		results = api.search(q="volumental-from%3Avolumental")
+		for result in results:
+			ts = datetime.fromtimestamp(mktime(strptime(str(result.created_at),'%Y-%m-%d %H:%M:%S')))
+			ts = ts + timedelta(hours=1)
+			near_past = datetime.now() - timedelta(minutes=1)
+			if (ts > near_past):
+				print "New tweet found! :", result.text.encode('utf-8')
+				sleep_for = 1
+				run_for = 5
+				slept = 0
+				while slept < run_for:
+					set_color(bridge, Color(config[u'colors'][u'twitter_blue']), config[u'groups'][u'twitter_lights'][u'ids'])
+					sleep(sleep_for)
+					set_color(bridge, Color(config[u'colors'][u'twitter_orange']), config[u'groups'][u'twitter_lights'][u'ids'])
+					slept += sleep_for
+		set_color(bridge, Color(config[u'colors'][u'volumental_mint']), config[u'groups'][u'twitter_lights'][u'ids'])
+
+
 def update_lamps(config, now):
-	bridge = create_bridge(config[u'bridge'][u'host'])
-	bridge.connect()
-	bridge.get_api()
-
-	today20 = now.replace(hour=20, minute=0, second=0, microsecond=0)
-	today06 = now.replace(hour=6, minute=0, second=0, microsecond=0)
-
-	if now > today06 and now < today20:
-
-		tc = create_team_city_client(config)
-		all_projects = tc.get_all_projects().get_from_server()
-
-		watched = config[u'teamcity'][u'watch'];
-
-		ok_projects = []
-		for p in all_projects[u'project']:
-			id = p[u'id'];
-			project = tc.get_project_by_project_id(id).get_from_server()
-	
-			if id in watched:
-				statuses = []	
-				for build_type in project[u'buildTypes'][u'buildType']:
-					b = tc.get_all_builds().set_build_type(build_type[u'id']).set_lookup_limit(2).get_from_server()
-					if u'build' in b:
-						status = b[u'build'][0][u'status']
-						statuses.append(status)
-		
-				ok_projects.append(not 'FAILURE' in statuses)
-
-		on(bridge)
-		if all(ok_projects):
-			set_color(bridge, Color(config[u'colors'][u'success']), config[u'groups'][u'build_lights'][u'ids'])
-		else:
-			set_color(bridge, Color(config[u'colors'][u'fail']), config[u'groups'][u'build_lights'][u'ids'])
+	try:
+		bridge = create_bridge(config[u'bridge'][u'host'])
+	except:
+		alarm.trigger()
 	else:
-		off(bridge)
-       
-	# Check if have a tweet in the last 5 minutes on twitter. If yes, let the twitter light go bananas.
-	auth = OAuthHandler(config[u'twitter_settings'][u'consumer_key'], config[u'twitter_settings'][u'consumer_secret'])
-	auth.set_access_token(config[u'twitter_settings'][u'access_token'],config[u'twitter_settings'][u'access_token_secret'])
-	api = API(auth)
-	results = api.search(q="volumental-from%3Avolumental")
-	for result in results:
-		ts = datetime.fromtimestamp(mktime(strptime(str(result.created_at),'%Y-%m-%d %H:%M:%S')))
-		ts = ts + timedelta(hours=1)
-		near_past = datetime.now() - timedelta(minutes=1)
-		if (ts > near_past):
-			print "New tweet found! :", result.text.encode('utf-8')
-			sleep_for = 1
-			run_for = 5
-			slept = 0
-			while slept < run_for:
-				set_color(bridge, Color(config[u'colors'][u'twitter_blue']), config[u'groups'][u'twitter_lights'][u'ids'])
-				sleep(sleep_for)
-				set_color(bridge, Color(config[u'colors'][u'twitter_orange']), config[u'groups'][u'twitter_lights'][u'ids'])
-				slept += sleep_for
-	set_color(bridge, Color(config[u'colors'][u'volumental_mint']), config[u'groups'][u'twitter_lights'][u'ids'])
+		bridge.connect()
+		bridge.get_api()
+
+		today20 = now.replace(hour=20, minute=0, second=0, microsecond=0)
+		today06 = now.replace(hour=6, minute=0, second=0, microsecond=0)
+
+		if now > today06 and now < today20:
+			try:
+				update_build_lamps(config, bridge)
+			except:
+				alarm.trigger()
+			try:					
+				update_twitter_lamps(config, bridge)
+			except: 
+				alarm.trigger(2)
+		else:
+			off(bridge)
 
 def main():
 	with open('config.json') as config_file:    
