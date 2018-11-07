69c54b85be52c5f7d6d66528c87e4354a04c75ad
Samuel Carlsson <samuel.carlsson@volumental.com>
2018-11-07 13:56:51 +0100

    disable button during trigger

--- static/build-lamp.js
+++ static/build-lamp.js
@@ -13,17 +13,15 @@ function show(element) {
   element.style.display = 'block';
 }
 
-function hide_progress() {
-  hide(document.getElementById('progress'));
-}
-
-function show_progress() {
-  show(document.getElementById('progress'));
-}
-
 function trigger() {
-  show_progress()
-  post('/api/trigger').then(hide_progress);
+  var progress = document.getElementById('progress');
+  var button = this;
+  show(progress);
+  button.disabled = true;
+  post('/api/trigger').finally(function() {
+    hide(progress);
+    button.disabled = false;
+  });
 }
 
 function ready() {
